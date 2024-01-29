6371b0cfef3ffe9225e786eb88c5c1dc78b10421
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-01-29 10:49:03 +0600

    dns

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-try {
-  const a = 1
-  const c = a + b
-} catch (err) {
-  console.log(err)
-}
+var dns = require("dns")
 
-const fs = require("fs")
-function nodeStyleCallback(err, data) {
-  if (err) {
-    console.error("There was an error", err)
-    return
-  }
-  console.log(data.toString())
-}
-fs.readFile("does-not-exist.txt", nodeStyleCallback)
-fs.readFile("does-exist.txt", nodeStyleCallback)
+console.log(dns.getServers())
